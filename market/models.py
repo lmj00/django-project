d784cb5d4ce189daa56df33957a0ac5775656af1
이민진 <e.minjin00@gmail.com>
2022-03-16 21:33:13 +0900

    Feat: models 닉네임 필드에 validators, error_messages 추가

--- market/models.py
+++ market/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
+from .validators import validate_no_special_characters
 
+# Create your models here.
 class User(AbstractUser):
-    nickname = models.CharField(max_length=15, unique=True, null=True)
+    nickname = models.CharField(
+        max_length=15, 
+        unique=True, 
+        null=True,
+        validators=[validate_no_special_characters],
+        error_messages={'unique': '이미 사용중인 닉네임입니다.'},
+    )
 
 
     def __str__(self):
