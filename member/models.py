0d3f3dce0e67df4f913562dfa6d5bdc7ed503a65
이민진 <e.minjin00@gmail.com>
2022-03-22 23:02:09 +0900

    Feat: models에 address 필드 추가

---
+++ member/models.py
@@ -0,0 +1,24 @@
+from ast import Pass
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .validators import validate_no_special_characters
+
+# Create your models here.
+class User(AbstractUser):
+    nickname = models.CharField(
+        max_length=15, 
+        unique=True, 
+        null=True,
+        validators=[validate_no_special_characters],
+        error_messages={'unique': '이미 사용중인 닉네임입니다.'},
+    )
+
+    address = models.CharField(
+        max_length=15,
+        null=True,
+    )
+
+
+    def __str__(self):
+        return self.email
+    
