107d597804c5264277effa02924ef7cf643b9b15
이민진 <e.minjin00@gmail.com>
2022-04-05 23:52:14 +0900

    Feat: view에 ajax로 받은 위도, 경도 값 session에 저장

--- member/views.py
+++ member/views.py
@@ -1,13 +1,26 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 from allauth.account.views import PasswordChangeView
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 class CustomPasswordChangeView(PasswordChangeView):
     def get_success_url(self):
         return reverse('index')
 
-
-def popupAddress(request):    
+def popupAddress(request): 
     return render(request, 'member/popup_address.html') 
 
+@csrf_exempt
+def test(request):   
+    jsonObject = json.loads(request.body)
+    # print(jsonObject)
+    if jsonObject.get('session') is not None:
+        request.session['lat'] = jsonObject.get('session').split(' ')[0]
+        request.session['lon'] = jsonObject.get('session').split(' ')[1]
+        # request.session['lat'] = None
+        # request.session['lon'] = None
+    
+    return JsonResponse(jsonObject)
