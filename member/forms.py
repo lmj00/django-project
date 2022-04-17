f04bcf3d38d1db8c81e746cde67689785f04d2b9
이민진 <e.minjin00@gmail.com>
2022-04-18 03:53:16 +0900

    Feat: forms.py request.session addr로 변경

--- member/forms.py
+++ member/forms.py
@@ -5,11 +5,11 @@ class SignupForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['nickname', 'address']
-    
+
 
     def signup(self, request, user):
         user.nickname = self.cleaned_data['nickname']
-        user.address = self.cleaned_data['address']
+        user.address = request.session['addr']
         user.lat = float(request.session['lat'])
         user.lon = float(request.session['lon'])
         user.save()
