f0b15042f2ca33d23e20df66908dd3dc47aaa7da
이민진 <e.minjin00@gmail.com>
2022-04-18 03:57:03 +0900

    Feat: ajax 주소 유효성 검사 path 추가

--- member/urls.py
+++ member/urls.py
@@ -1,7 +1,10 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
+
 urlpatterns = [ 
     path('popup_address/',  views.popupAddress, name='popup_address'),
-    path('popup_address/test', views.test, name='test')
+    path('popup_address/test', views.test, name='test'),
+    path('signup/addrCheck', views.signupAddressCheck, name='address_check'),
 ]
