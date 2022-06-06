b4c961759c622fe740b77b790f2a44fe8f32a27e
이민진 <e.minjin00@gmail.com>
2022-06-07 01:41:11 +0900

    Feat: shop models POST 클래스 추가

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,31 @@
+from email.mime import image
 from django.db import models
+from django.forms import CharField
+from django.core.validators import MinValueValidator
+from member.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+
+    content = models.TextField()
+    
+    price = models.IntegerField(
+        validators=[MinValueValidator(1)]
+    ) 
+
+    image1 = models.ImageField()
+    image2 = models.ImageField(blank=True)
+    image3 = models.ImageField(blank=True)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    dt_created = models.DateTimeField(auto_now=True)
+
+
+    """ 
+    카테고리 추가 예정
+    """
+
+    def __str__(self):
+        return self.title
