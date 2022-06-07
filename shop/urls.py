c63aa2586f8e216bbbdf146c2f0c2a6d1f7db352
이민진 <e.minjin00@gmail.com>
2022-06-08 06:04:27 +0900

    Feat: post PostView로 추가

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [ 
-    path('', views.IndexView.as_view(), name='index'),
+urlpatterns = [
+    path('', views.IndexView.as_view(), name = 'index'),
+    path('posts', views.PostView.as_view(), name = 'posts')
 ]
