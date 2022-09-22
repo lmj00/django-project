8fa1da3d72ac00df7491574ca577b27ebf7bc56b
이민진 <e.minjin00@gmail.com>
2022-09-23 01:32:09 +0900

    Feat: chat model에 room 클래스 추가

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from member.models import User
+from shop.models import Post
 
 # Create your models here.
+class Room(models.Model):
+    # post_id = models.ForeignKey(Post, on_delete=models.PROTECT)
+    # user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_id = models.IntegerField()
+    user_id = models.IntegerField()
+    last_content = models.TextField(blank=True)
+
+
+class Message(models.Model):
+    roomname = models.ForeignKey(Room, on_delete=models.PROTECT)
+    content = models.TextField()       
+    post = models.ForeignKey(Post, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestmap = models.DateTimeField(auto_now=True)
