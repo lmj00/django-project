0d12ec6341ac4987eb05e3b14016aa05f51df0bf
이민진 <e.minjin00@gmail.com>
2022-09-25 05:20:59 +0900

    Feat: chat views 추가
    채팅방 번호를 가져온다.

--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from shop.models import Post
+from chat.models import Room
 
 # Create your views here.
-def room(request, post_id):
-    post = Post.objects.get(id__icontains=post_id)
+def room(request, post_id, user_id): 
     
+    post = Post.objects.get(id=post_id)
+    check_buyer = len(Room.objects.filter(buyer_id=user_id))
+
+    if check_buyer == 0:
+        Room.objects.create(
+            post_id = post_id, 
+            seller_id = post.author.id, 
+            buyer_id = user_id,
+            last_content = ""
+        )
+    
+
+    if post.author.id != user_id:
+        room = Room.objects.get(post_id=post_id, buyer_id=user_id)
+
+
     return render(request, 'chat/room.html', {
         'post_id': post_id,
-        'post_author_id': post.author.id
+        'room': room.id
     })
