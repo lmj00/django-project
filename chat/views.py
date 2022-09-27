a9f596aa7e915f12cc1a6630e2c568dcc81746b3
이민진 <e.minjin00@gmail.com>
2022-09-28 02:45:52 +0900

    Feat: chat view 채팅방 개설 누락 조건 추가

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,3 @@
-from turtle import title
 from django.shortcuts import render
 from shop.models import Post
 from chat.models import Message, Room
@@ -8,7 +7,7 @@ from django.views.generic import ListView
 # Create your views here.
 def room(request, post_id, user_id): 
     post = Post.objects.get(id=post_id)
-    room = Room.objects.filter(buyer_id=user_id)   
+    room = Room.objects.filter(buyer_id=user_id, seller_id=post.author.id)   
 
     # 채팅방 개설
     if len(room) == 0:
@@ -24,7 +23,7 @@ def room(request, post_id, user_id):
         Q(seller_id=post.author.id) & Q(buyer_id=user_id)
     )   
 
-        
+    
     return render(request, 'chat/room.html', {
         'post_id': post_id,
         'room': room.id
