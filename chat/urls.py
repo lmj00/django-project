743fda7b40dbcada8b41a3b61cb8a2bee68f6a2f
이민진 <e.minjin00@gmail.com>
2022-09-28 01:02:52 +0900

    Feat: chat urls
    채팅하기 path 수정, 채팅방 목록 path 추가

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,18 @@
 # chat/urls.py
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('<str:post_id>/', views.room, name='room'),
+    path(
+        '<int:post_id>/<int:user_id>/', 
+        views.room, 
+        name='room'
+    ),
+    
+    path(
+        '<int:user_id>/', 
+        views.RoomList.as_view(), 
+        name='room_list'
+    ),
+
 ]
