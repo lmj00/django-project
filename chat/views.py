1862cb8ce9fb3144314eec56c1879c4a559831e4
이민진 <e.minjin00@gmail.com>
2022-09-28 01:04:03 +0900

    Feat: chat views
    채팅방 개설 로직 수정, 채팅방 목록 class 추가

--- chat/views.py
+++ chat/views.py
@@ -1,27 +1,45 @@
+from turtle import title
 from django.shortcuts import render
 from shop.models import Post
-from chat.models import Room
+from chat.models import Message, Room
+from django.db.models import Q
+from django.views.generic import ListView
 
 # Create your views here.
 def room(request, post_id, user_id): 
-    
     post = Post.objects.get(id=post_id)
-    check_buyer = len(Room.objects.filter(buyer_id=user_id))
+    room = Room.objects.filter(buyer_id=user_id)   
 
-    if check_buyer == 0:
-        Room.objects.create(
+    # 채팅방 개설
+    if len(room) == 0:
+            Room.objects.create(
             post_id = post_id, 
             seller_id = post.author.id, 
             buyer_id = user_id,
             last_content = ""
         )
-    
-
-    if post.author.id != user_id:
-        room = Room.objects.get(post_id=post_id, buyer_id=user_id)
 
 
+    room = Room.objects.get(
+        Q(seller_id=post.author.id) & Q(buyer_id=user_id)
+    )   
+
+        
     return render(request, 'chat/room.html', {
         'post_id': post_id,
         'room': room.id
     })
+
+
+class RoomList(ListView):
+    model = Room
+    template_name = 'chat/room_list.html'    
+
+    def get_queryset(self):
+        user_id = self.request.user.id
+        room = Room.objects.filter(
+            Q(seller_id=user_id) | Q(buyer_id=user_id)
+        )   
+
+        return room
+    
