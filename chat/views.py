eb83127cada3d1ccfd187e1bce6256c2dd9717f1
이민진 <e.minjin00@gmail.com>
2022-09-29 03:44:26 +0900

    Feat: 저장된 채팅 내용 불러오기 추가

--- chat/views.py
+++ chat/views.py
@@ -11,23 +11,26 @@ def room(request, post_id, user_id):
 
     # 채팅방 개설
     if len(room) == 0:
-            Room.objects.create(
+        Room.objects.create(
             post_id = post_id, 
             seller_id = post.author.id, 
             buyer_id = user_id,
             last_content = ""
         )
-
-
+        
     room = Room.objects.get(
         Q(seller_id=post.author.id) & Q(buyer_id=user_id)
     )   
 
+    message = Message.objects.filter(roomname_id=room.id)
+
     
     return render(request, 'chat/room.html', {
-        'post_id': post_id,
-        'room': room.id
+        'post_id': post_id, 
+        'room': room.id, 
+        'message_list': message 
     })
+
 
 
 class RoomList(ListView):
