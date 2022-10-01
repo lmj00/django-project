57934d5cd48258cb4d2b555477b771e6613a51ff
이민진 <e.minjin00@gmail.com>
2022-10-02 06:10:05 +0900

    Feat: member middleware 추가

--- market_project/settings.py
+++ market_project/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'member.middleware.ProfileSetupMiddleware'
 ]
 
 ROOT_URLCONF = 'market_project.urls'
