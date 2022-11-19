5305323a9b51f97dd2d49deb6fabcb51037f18d7
이민진 <e.minjin00@gmail.com>
2022-11-20 03:35:47 +0900

    heroku redis error fix

--- market_project/settings.py
+++ market_project/settings.py
@@ -188,7 +188,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('REDIS_URL', 'redis://localhost:6379')]
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
     },
 }
